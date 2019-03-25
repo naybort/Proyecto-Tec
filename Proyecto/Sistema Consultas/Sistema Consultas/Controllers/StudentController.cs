cd890afd08c479dd8b5ac6054886f1379c5ef448
marcer67 <mcorear97@gmail.com>
2019-03-25 01:39:17 -0600

    Cambio de core

---
+++ Proyecto/Sistema Consultas/Sistema Consultas/Controllers/StudentController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sistema_Consultas.Controllers
+{
+    public class StudentController : Controller
+    {
+        public ActionResult Thematics()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
