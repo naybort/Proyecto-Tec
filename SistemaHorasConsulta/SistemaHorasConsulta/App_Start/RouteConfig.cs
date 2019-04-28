5c64b136d1d171dcac09f7411ba9ef32a080e0a0
marcer67 <mcorear97@gmail.com>
2019-04-27 22:11:43 -0600

    Update a vista de login, logo y dimensiones

--- SistemaHorasConsulta/SistemaHorasConsulta/App_Start/RouteConfig.cs
+++ SistemaHorasConsulta/SistemaHorasConsulta/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace SistemaHorasConsulta
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Autenticacion", id = UrlParameter.Optional }
             );
         }
     }
